44859a76a6383434cbbc08dcdac45a5af4b2d1d2
Victor <vic500@gmail.com>
2023-09-05 22:37:23 +0300

    task 3

---
+++ Sem1Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задание 2. Определить день недели по номеру
+Console.WriteLine("Enter day");
+int day = int.Parse(Console.ReadLine ()??"0");
+string [] dayOfWeek = new string [7]; // создание массива dayOfWeek длинной 7
+    dayOfWeek[0] = "Понедельник";
+    dayOfWeek[1] = "Вторник";
+    dayOfWeek[2] = "Среда";
+    dayOfWeek[3] = "Четверг";
+    dayOfWeek[4] = "Пятница";
+    dayOfWeek[5] = "Суббота";
+    dayOfWeek[6] = "Воскресение";
+Console.WriteLine(dayOfWeek [day-1]);
