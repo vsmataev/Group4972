dfc2a6f61fb03fcec288d58f7ea82fc6fd3403fc
Victor <vic500@gmail.com>
2023-09-05 17:19:52 +0300

    задание2

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задание 2: Является ли первое число квадрвтом второго
+
+Console.WriteLine("Введите первое  число ");
+
+int num1 = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Введите второе число ");
+
+int num2 = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine(num1 == Math.Pow(num2, 2) ? "Да" : "Нет"); // тернарный оператор
+
+// <условие>? <вариант1> : <вариант2> 
+//эквивалентно:
+// if(num1 == Math.Pow(num2, 2))
+// {
+
+//     Сonsole.WriteLine("Да");
+
+// }
+// else
