7d4f04b15e8bc8acf6e8fb3dd2dfe2c19cb004a3
Victor <vic500@gmail.com>
2023-09-05 15:57:10 +0300

    Заработало

---
+++ Sem1Task0/Program.cs
@@ -0,0 +1,16 @@
+﻿// квадрат числа
+
+Console.WriteLine("введите число: "); //вводим число
+string? InputNum = Console.ReadLine();//??"0"; // string? означает что сравнение с null будет ниже
+
+if(InputNum!=null) // знак != означает неравенство
+{
+
+    int num = int.Parse(InputNum); // Parse - перевод значения в числовую форму
+
+    int res = num*num; //вычисление квадрата числа
+
+    Console.WriteLine(res); //результат
+}
+
+Console.WriteLine ($"квадрат числа {InputNum} равен "+Math.Pow(int.Parse(InputNum),2)); //через функцию Math.Pow
