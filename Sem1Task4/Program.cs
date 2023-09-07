b8f5c2688abdf00d426de9459502016bfda79277
Victor <vic500@gmail.com>
2023-09-07 23:32:21 +0300

    ДЗ 4

---
+++ Sem1Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿// ДЗ №4 Напишите программу, которая принимает на вход три числа и выдаёт максимальное из
+// этих чисел.
+Console.Write("ведите первое число: ");
+int? firstNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int? secondNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int? thirdNum = Convert.ToInt32(Console.ReadLine());
+int? max = firstNum;
+if(firstNum == secondNum && firstNum == thirdNum)
+{
+    Console.WriteLine("все числа равны");
+}
+    else
+{
+        if(max < secondNum)
+{      
+             max = secondNum;
+}
+        if(max < thirdNum)
+{    
+            max = thirdNum;
+}
+Console.WriteLine($"наибольшее число {max}");
+}
