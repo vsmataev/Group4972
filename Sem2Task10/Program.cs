791e03d3ce580e6bd7d66e2969bc54e69c8a763c
Victor <vic500@gmail.com>
2023-09-14 21:06:12 +0300

    Sem2Task10 new

---
+++ Sem2Task10/Program.cs
@@ -0,0 +1,10 @@
+﻿// №10 Напишите программу, 
+// которая принимает на вход 
+// трёхзначное число и на выходе 
+// показывает вторую цифру этого числа.
+
+  System.Random numberGenerator = new System.Random(); //генератор случайных чисел
+    int number = numberGenerator.Next(100,1000); //случайное трехзначное число
+    Console.WriteLine (number);
+    int thirdNum = number/10 %10; //последовательно убираем последнюю и первую цифры
+    Console.WriteLine($"Вторая цифра это {thirdNum}");
