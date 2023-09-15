e5c488d0f0341d6ab3b0660c454db70d13d3b650
vsmataev <vsmataev@bakulev.ru>
2023-09-15 09:23:22 +0300

    Task13

--- Sem2Task13/Program.cs
+++ Sem2Task13/Program.cs
@@ -13,17 +13,18 @@ Console.WriteLine("Введите число от 1 до 9999999999"); // пус
  bool test = (number<100);
 if (test) Console.WriteLine("Число не трехзначное");
 else if (number > 99)
+{
     numbers.Add(i);
-while (temp2%10==0);
+}
+Count i;
+if (temp1%10==0)
 {
 temp1 /= (int)temp2%10; //5,6
 i = (int)temp1; //5,4
 temp2 /= (int)temp2/10; //76,7
 numbers.Add(i);
 temp1 = (int)temp2; //76,7
-if(i>=1);
 }
-return temp2; //7
+return //temp2; //7
 
-//return temp1*1
 Console.WriteLine(numbers.Count);
