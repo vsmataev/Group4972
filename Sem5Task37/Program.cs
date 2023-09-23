9823f6c92b2a07d49046bbe37f68b78e46dc2b28
Victor <vic500@gmail.com>
2023-09-24 00:57:58 +0300

    Sem5 all tasks

---
+++ Sem5Task37/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний
+// и т.д. Результат запишите в новом массиве.
+
+int[] GenArray(int num, int lowBorder, int highBorder)
+{
+    int[] array = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = new Random().Next(lowBorder, highBorder + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    string arrayString = "[" + string.Join( ",", arr) + "]"; //выводим массив красиво
+    Console.WriteLine(arrayString);
+}
+
+int[] MultiPair(int[] array)
+{
+    int[] pairMass = new int[array.Length/2];
+    for (int i = 0; i < array.Length/2; i++)
+    {        
+        pairMass[i] = array[i] * array[array.Length-1 - i];
+    }
+    return pairMass;
+}
+
+// void PrintRezult(string msg)
+// {
+//     System.Console.WriteLine(msg);
+// }
+
+int[] arr = GenArray(12, -9, 9);
+PrintArray(arr);
+PrintArray(MultiPair(arr));
