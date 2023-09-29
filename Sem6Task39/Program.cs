5b1903c02cb1464977d7cc0b31462cf0c2b9301e
Victor <vic500@gmail.com>
2023-09-30 00:32:16 +0300

    Sem6AllTasks

---
+++ Sem6Task39/Program.cs
@@ -0,0 +1,61 @@
+﻿//Задача №39
+//Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте,
+// а первый - на последнем и т.д.)
+//[1 2 3 4 5] -> [5 4 3 2 1]
+
+// Решение двумя разными методами
+// Генерация массива
+int[] GenerateRandomArray(int length, int minValue, int maxValue)
+{
+    int[] array = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++) { array[i] = rnd.Next(minValue, maxValue + 1); }
+    return array;
+}
+
+// Вывод массива
+void PrintArray(string msg, int[] array)
+{
+    Console.Write(msg + "[");
+    for (int i = 0; i < array.Length - 1; i++) { Console.Write(array[i] + ", "); }
+    Console.WriteLine(array[array.Length - 1] + "]");
+}
+
+
+
+int[] SwapNewArray(int[] arr) //способ 1 - создает новый массив и заполняет его в обратном порядке
+{
+    int[] outArr = new int[arr.Length]; // новый массив длинной равный входному
+    for(int i =0;i<arr.Length;i++)
+    {
+        outArr[arr.Length-1 - i] = arr[i];
+    }
+    return outArr;
+}
+
+
+void SwapArray(int[] arr) // способ 2 - переворачивает исходный массив,
+//закоментирован код с использованием промежуточного элемента
+{
+   // int buferElement = 0;
+    for(int i =0; i<arr.Length/2;i++)
+    {
+        (arr[arr.Length-1 - i],arr[i])=(arr[i], arr[arr.Length-1 - i]); //буферная переменная не инциилизуется, 
+                                                                        //но атоматически создается
+        // buferElement = arr[arr.Length-1 - i];
+        // arr[arr.Length-1 - i]=arr[i];
+        // arr[i] = buferElement;
+    }
+}
+
+int[] arr = GenerateRandomArray(15,1,100);
+PrintArray("",arr);
+int[] swapArr = SwapNewArray(arr);
+Console.WriteLine("Исходный массив");
+PrintArray("",arr);
+Console.WriteLine("Перевернутый массив");
+PrintArray("",swapArr);
+
+SwapArray(arr);
+Console.WriteLine("Исходный массив");
+PrintArray("",arr);
---
+++ Sem6Task44/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача №44
+// Не используя рекурсию, выведите первые N чисел
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+
+
+//------------------------------------------------------------------------------------------------------------------
+Console.Clear();
+int n = ReadData("Введите количество чисел Фибоначчи (N): ");
+PrintFibonacciNumbers(n);
+
+//                                                     Методы
+//------------------------------------------------------------------------------------------------------------------
+// Ввод числа с клавиатуры
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Метод печати чисел Фибоначчи
+void PrintFibonacciNumbers(int n)
+{
+    int first = 0;
+    int second = 1;
+
+    Console.Write(first + " " + second + " ");
+
+    for (int i = 2; i < n; i++)
+    {
+        int next = first + second;
+        Console.Write(next + " ");
+
+        first = second;
+        second = next;
+    }
+}
